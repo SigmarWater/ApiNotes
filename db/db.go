657b68824a705660e45bd97ac5a0c141037e05ec
Udalcov Artem <artemudalcov05@gmail.com>
2025-07-20 16:59:01 +0300

    added db

--- db/db.go
+++ db/db.go
@@ -1,6 +1,52 @@
-package db 
+package db
 
-import(
-	_ ""
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"log"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+
+var DB *sql.DB 
+var err error
+var connStr string
+
+func init(){
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	host := os.Getenv("HOST")
+	port := os.Getenv("PORT")
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	connStr = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=notes sslmode=disable", host, port, user, password)
+}
+
+
+
+
+func ConnectDB(){
+
+	DB, err = sql.Open("postgres", connStr)
+
+	if err != nil{
+		log.Println("error open DB")
+		return 
+	}
+
+	err = DB.Ping()
+	if err != nil{
+		log.Println(err.Error())
+		return
+	}
+
+	log.Println("succses connection")
+}
+
+
 
--- main.go
+++ main.go
@@ -4,82 +4,85 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"time"
 	"sync"
+	"time"
+	"github.com/SigmarWater/ApiNotes/db"
 
 	"github.com/gin-gonic/gin"
 )
 
-type Note struct{
-	ID int64 `json:"id"`
-	Data time.Time `json:"data" binding:"omitempty"`
-	Title string `json:"title"`
+func init(){
+	db.ConnectDB()
+}
+
+type Note struct {
+	ID    int64     `json:"id"`
+	Date  time.Time `json:"date" binding:"omitempty"`
+	Title string    `json:"title"`
 }
 
 var mu sync.RWMutex
 
 var notes map[int64]Note = map[int64]Note{}
 
-func main(){
+func main() {
 	router := gin.Default()
 
-	router.GET("/notes", func(c *gin.Context){
-		note_arr  := make([]Note, 0, len(notes))
-		for _, note := range notes{
+	router.GET("/notes", func(c *gin.Context) {
+		note_arr := make([]Note, 0, len(notes))
+		for _, note := range notes {
 			note_arr = append(note_arr, note)
 		}
-		c.JSON(http.StatusOK, gin.H{"notes":note_arr})
+		c.JSON(http.StatusOK, gin.H{"notes": note_arr})
 	})
 
-	router.GET("/notes/:id", func(c *gin.Context){
-		id, err :=  strconv.ParseInt(c.Param("id"), 0, 64)
-		if err != nil{
+	router.GET("/notes/:id", func(c *gin.Context) {
+		id, err := strconv.ParseInt(c.Param("id"), 0, 64)
+		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "incorrect id"})
 			return
 		}
 
 		mu.RLock()
-		if note, ok := notes[id]; ok{
+		if note, ok := notes[id]; ok {
 			c.JSON(http.StatusOK, note)
-		}else{
+		} else {
 			c.JSON(http.StatusNotFound, gin.H{"error": "not found note"})
 		}
 		mu.RUnlock()
 	})
 
-	router.POST("/notes", func(c *gin.Context){
-		
+	router.POST("/notes", func(c *gin.Context) {
 
 		var note Note
-		if err := c.ShouldBindJSON(&note); err != nil{
+		if err := c.ShouldBindJSON(&note); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "bad body"})
 			return
 		}
-		
+
 		mu.RLock()
-		
+
 		_, ok := notes[note.ID]
 
 		mu.RUnlock()
 
-		if ok{
+		if ok {
 			c.JSON(http.StatusBadRequest, fmt.Sprintf("Заметка с id=%d уже есть", note.ID))
 			return
 		}
 
-		if note.Title == ""{
+		if note.Title == "" {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "field Title is empty"})
-			return 
+			return
 		}
-		
+
 		mu.Lock()
-		note.Data = time.Now()
+		note.Date = time.Now()
 		notes[note.ID] = note
 		mu.Unlock()
 		c.JSON(http.StatusCreated, note)
-	
-	})
 
+	})
 
 	router.Run()
 }
